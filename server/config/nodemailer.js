524f6eb55505b196118ff588530f256cdd8da205
Akshat <s.akshat340@gmail.com>
2025-12-11 16:42:46 +0530

    Minor fixes

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,13 +1,19 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", 
-    port: 465,              
-    secure: true,          
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.SENDER_EMAIL, 
-        pass: process.env.SMTP_PASS, 
-    }
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SMTP_PASS,
+    },
+   
+    family: 4,              // Force IPv4 (Render ipv6 issue fix)
+    connectionTimeout: 10000, // 10 seconds wait before giving up
+    logger: true,           // Logs mein details dikhayega
+    debug: true             // Debug info print karega
+
 });
 
 export default transporter;
