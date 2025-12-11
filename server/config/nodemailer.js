22294eedea7ee15508194323960801509e65515e
Akshat <s.akshat340@gmail.com>
2025-12-11 19:54:29 +0530

    Minor fixes

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,15 +1,10 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,              // 465 ki jagah 587 use karenge
-    secure: false,          // 587 ke liye ye false hona chahiye (STARTTLS)
+    service: 'gmail', 
     auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SMTP_PASS,
-    },
-    tls: {
-        rejectUnauthorized: false // Cloud server certificate issues ko bypass karega
+        user: process.env.SENDER_EMAIL, 
+        pass: process.env.SMTP_PASS, 
     }
 });
 
