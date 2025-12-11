4212f1562466e526727e3575877a8b24c03aa569
Akshat <s.akshat340@gmail.com>
2025-12-11 16:35:52 +0530

    Minor fixes

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', 
+    host: "smtp.gmail.com", 
+    port: 465,              
+    secure: true,          
     auth: {
         user: process.env.SENDER_EMAIL, 
-        pass: process.env.SMTP_PASS,   
+        pass: process.env.SMTP_PASS, 
     }
 });
 
